56d7b576ab20cf676d6e0f8eecc3db95455c992b
inderdeep26 <inderdeep2626@gmail.com>
2025-10-08 13:19:12 -0400

    message

---
+++ tutorial-09-js-main/tny_script.js
@@ -0,0 +1,19 @@
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 9
+   Tutorial Case
+
+   Countdown Clock
+   Author: Inderdeep Singh
+   Date:   2025-10-08
+
+*/
+
+//Display the current date and time
+document.getElementById("dateNow").innerHTML ="n/d/ycbr />h:m:S";
+
+//display the time left until new year eve bash
+document.getElementById('days').textContent="dd";
+document.getElementById('hrs').textContent="hh";
+document.getElementById('mins').textContent="mm";
+document.getElementById('secs').textContent="ss";
